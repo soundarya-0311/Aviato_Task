ab490b963bf412d652a85b6ff60b26faa8f68771
Soundarya <soundarya.g@innoboon.com>
2024-12-02 23:56:24 +0530

    firestore gcp connected

--- database/database.py
+++ database/database.py
@@ -1,5 +1,5 @@
 from google.cloud import firestore
 
-client = firestore.Client()
+client = firestore.Client.from_service_account_json("phrasal-chiller-443513-u1-1eb39beb218a.json")
 
 db = client.collection("users")
--- routers/routers.py
+++ routers/routers.py
@@ -9,21 +9,21 @@ router = APIRouter()
 @router.post("/add_users")
 def create_users(user: Users):
     try:
-        add_new_user = db.add(
+        timestamp, add_new_user = db.add(
             {
                 "username" : user.username,
                 "email" : user.email,
                 "project_id" : user.project_id
             }
         )
-        
+
         return JSONResponse(
             status_code = status.HTTP_200_OK,
             content = {"message" : "User Added Successfully",
                        "user_id" : add_new_user.id, 
-                       "username": add_new_user.username, 
-                       "email": add_new_user.email,
-                       "project_id" : add_new_user.project_id}
+                       "username": user.username, 
+                       "email": user.email,
+                       "project_id" : user.project_id}
         )
         
     except Exception:
@@ -70,6 +70,33 @@ def update_users(user_id: str, user: Users):
             content = {"message" : "Updated Successfully"}
         )
             
+    except HTTPException as e:
+        traceback.print_exc()
+        return JSONResponse(
+            status_code=e.status_code,
+            content = e.detail
+        )
+    
+    except Exception:
+        traceback.print_exc()
+        return JSONResponse(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            content = {"message" : "Something Went Wrong"}
+        )
+
+@router.delete("/delete_users")
+def delete_users(user_id: str):
+    try:
+        user_ref = db.document(user_id)
+        user_data = user_ref.get()
+        if not user_data:
+            raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail = "User Details Not Found")
+        user_ref.delete()
+        return JSONResponse(
+            status_code = status.HTTP_200_OK,
+            content = {"message" : "Deleted Successfully"}
+        )
+        
     except HTTPException as e:
         traceback.print_exc()
         return JSONResponse(
